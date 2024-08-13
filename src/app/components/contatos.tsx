1109dc4743273afe8c66a071c1eefd7ac65d5140
AllanOSilva <154864238+AllanOSilva@users.noreply.github.com>
2024-08-13 19:15:15 -0300

    primeiro commit

---
+++ src/app/components/contatos.tsx
@@ -0,0 +1,11 @@
+"use client"
+
+export default function Contatos(){
+    return (
+        <>
+        <div className="bg-blue-800 text-gray-200 p-1">
+            Olá mundo!
+        </div>
+        </>
+    )
+}
---
+++ src/app/components/navbar.tsx
@@ -0,0 +1,11 @@
+"use client"
+
+export default function Navbar(){
+    return (
+        <>
+        <div className="bg-blue-600 text-gray-200">
+            oi
+        </div>
+        </>
+    )
+}
