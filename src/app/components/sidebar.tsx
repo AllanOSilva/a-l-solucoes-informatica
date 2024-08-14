2c05b8ea5cfc8e51e7b0c57c801315b6c2e661ef
AllanOSilva <154864238+AllanOSilva@users.noreply.github.com>
2024-08-13 22:29:01 -0300

    criando sidebar

---
+++ src/app/components/sidebar.tsx
@@ -0,0 +1,11 @@
+"use client"
+
+export default function Sidebar(){
+    return (
+        <>
+        <div className="bg-blue-500 text-gray-200 p-1">
+            oi
+        </div>
+        </>
+    )
+}
