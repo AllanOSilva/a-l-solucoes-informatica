02e3c31cee38c5269f0008cb20137dc9967bea1c
AllanOSilva <154864238+AllanOSilva@users.noreply.github.com>
2024-08-13 22:28:51 -0300

    mudando tags

--- src/app/components/contatos.tsx
+++ src/app/components/contatos.tsx
@@ -5,18 +5,18 @@ import { Phone } from 'lucide-react';
 export default function Contatos(){
     return (
         <>
-        <div className="bg-blue-600 text-gray-200 p-2 max-w-full">
-            <ul >
-                <li className='flex'>
+        <div className="bg-blue-600 text-gray-200 p-2 max-w-full justify-between " >
+            
+                <span className='flex'>
                     <Phone className='text-white' /><span className='px-2'>(21)99999-9999 |</span>
-                </li>
-                <li className='flex'>
-                    <i className ="fab fa-whatsapp" />
-                </li>
-                <li className='flex'>
-                    <i className ="fab fa-whatsapp" />
-                </li>
-            </ul>
+                </span>
+                <span className='flex'>
+                    
+                </span>
+                <span className='flex'>
+                    
+                </span>
+            
             
         </div>
         </>
