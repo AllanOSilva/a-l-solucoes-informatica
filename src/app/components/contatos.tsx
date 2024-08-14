39266a208a053e0772550d60294fafc3c3765b1b
AllanOSilva <154864238+AllanOSilva@users.noreply.github.com>
2024-08-13 22:28:37 -0300

    corrigindo erros

--- src/app/components/contatos.tsx
+++ src/app/components/contatos.tsx
@@ -1,10 +1,23 @@
 "use client"
+import { Phone } from 'lucide-react';
+
 
 export default function Contatos(){
     return (
         <>
-        <div className="bg-blue-600 text-gray-200 p-1">
-            Olá mundo!
+        <div className="bg-blue-600 text-gray-200 p-2 max-w-full">
+            <ul >
+                <li className='flex'>
+                    <Phone className='text-white' /><span className='px-2'>(21)99999-9999 |</span>
+                </li>
+                <li className='flex'>
+                    <i className ="fab fa-whatsapp" />
+                </li>
+                <li className='flex'>
+                    <i className ="fab fa-whatsapp" />
+                </li>
+            </ul>
+            
         </div>
         </>
     )
