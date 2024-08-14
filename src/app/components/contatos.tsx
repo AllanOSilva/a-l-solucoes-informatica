a164308bcbf3ef5a32869b7954d3506a01a73010
AllanOSilva <154864238+AllanOSilva@users.noreply.github.com>
2024-08-14 19:37:32 -0300

    adicionando whatsapp

--- src/app/components/contatos.tsx
+++ src/app/components/contatos.tsx
@@ -1,17 +1,17 @@
 "use client"
 import { Phone } from 'lucide-react';
+import Whatsapp from './ui/whatsapp';
 
 
 export default function Contatos(){
     return (
         <>
-        <div className="bg-blue-600 text-gray-200 p-2 max-w-full justify-between " >
-            
+        <div className="bg-blue-600 text-gray-200 p-2 max-w-full justify-around flex" >
                 <span className='flex'>
-                    <Phone className='text-white' /><span className='px-2'>(21)99999-9999 |</span>
+                    <Phone className='text-white' /><span className='px-2'>(21) 97041-6875 |</span>
                 </span>
                 <span className='flex'>
-                    
+                    <a href="https://wa.me/5521970416875"><Whatsapp /></a>
                 </span>
                 <span className='flex'>
                     
