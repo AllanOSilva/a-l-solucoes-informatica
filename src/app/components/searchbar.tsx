26b527a67b3b49de0baf5f361dc240707ffa7de2
AllanOSilva <154864238+AllanOSilva@users.noreply.github.com>
2024-08-19 21:29:42 -0300

    arrumando searchbar

--- src/app/components/searchbar.tsx
+++ src/app/components/searchbar.tsx
@@ -2,10 +2,10 @@
 export default function Searchbar(){
     return(
         <>
-<div className=" flex gap-3 md:w-60% place-self-center">
+<div className="flex gap-3 place-self-center ">
   <input
     type="search"
-    className="  rounded border border-solid border-neutral-200 bg-white bg-clip-padding px-3 py-[0.25rem] text-base font-normal leading-[1.6] text-surface outline-none transition duration-200 ease-in-out placeholder:text-gray-200 focus:z-[3] focus:border-primary focus:shadow-inset focus:outline-none motion-reduce:transition-none dark:border-white/10 dark:text-black dark:placeholder:text-neutral-400 dark:autofill:shadow-autofill dark:focus:border-primary"
+    className=" rounded border border-solid border-neutral-200 bg-white bg-clip-padding py-3 outline-none transition dark:text-black"
     id="exampleFormControlInput2"
     />
   <button type="button"
